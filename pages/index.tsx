0f8a5d6f0342cbed85e23e936b5d85ef182a6b65
ask03 <aykim043@gmail.com>
2024-03-10 22:37:46 -0400

    more fields

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,17 +31,25 @@ const Home: NextPage = () => {
                 align: "center",
               }}
             />
-
             {address ? (
               <div className={` ${styles.gradientText0} `}>
                 <h2>Points Details</h2>
                 <div className={`${styles.pointTypes}`}>
                 <p>Fanda</p>
+                <p>Dunkin</p>
                 <p>Ford Focus Group</p>
+                <p>Influencer Tier 1</p>
+                <p>Influencer Tier 2</p>
+                <p>Influencer Tier 3</p>
+                
                 </div>
                 <div className={`${styles.points}`}>
-                <p>25023</p>
-                <p>1232</p>
+                <p>23,950</p>
+                <p>7,000</p>
+                <p>5,378</p>
+                <p>549</p>
+                <p>0</p>
+                <p>0</p>
                 </div>
               </div>
             ) : (
